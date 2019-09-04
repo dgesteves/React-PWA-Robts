1a06c2197018cf39ff30b5433799713a64dd3dee
Diogo Esteves <diogo.esteves.goncalves@gmail.com>
2019-09-04 15:17:40 +0100

    Updates

---
+++ precache-manifest.16cde7855c02edfa28b0863b58fc7186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72acecd7a8f11c06a6b340e20f8fab91",
+    "url": "/React-PWA-Robts/index.html"
+  },
+  {
+    "revision": "dda04cac2daad91328f4",
+    "url": "/React-PWA-Robts/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "39549793dc3a43f4a49f",
+    "url": "/React-PWA-Robts/static/css/main.d468333d.chunk.css"
+  },
+  {
+    "revision": "dda04cac2daad91328f4",
+    "url": "/React-PWA-Robts/static/js/2.3ca47fc4.chunk.js"
+  },
+  {
+    "revision": "39549793dc3a43f4a49f",
+    "url": "/React-PWA-Robts/static/js/main.042c5e0b.chunk.js"
+  },
+  {
+    "revision": "6e296dfb66457dfab1a7",
+    "url": "/React-PWA-Robts/static/js/runtime~main.4b3b1a17.js"
+  }
+]);
